c36998a0ea7bbcfa0018597368445bbfda23f4ca
wesmith <smiwarsky@gmail.com>
2019-12-18 01:34:52 -0700

    added register set-up info for LSM6

--- pi/LSM6.py
+++ pi/LSM6.py
@@ -2,7 +2,7 @@
 
 '''
 WSmith copied this file from Pololu's a_star.py, to create a class to
-read the LSM6 accelerometer/magnetometer chip on the Pololu Balboa board,
+read the LSM6DS33 accelerometer/gyro chip on the Pololu Balboa board
 from the RPi.
 '''      
 
@@ -13,9 +13,15 @@ import time
 class LSM6:
   def __init__(self):
     self.bus = smbus.SMBus(1)
-    self.address  = 0x6b  # LSM6 I2C address
-    self.IDreg    = 0x0f  # register containing device ID
-    self.ID       = 0x69  # device ID
+    self.address     = 0x6b  # LSM6DS33 SAO_HIGH I2C address
+    self.WHO_AM_I    = 0x0f  # register containing device ID
+    self.DS33_WHO_ID = 0x69  # device ID
+    self.CTRL1_XL    = 0X10  # accelerometer buffer
+    self.CTRL2_G     = 0X11  # gyro          buffer
+    self.CTRL3_C     = 0X12  # common        buffer
+    self.OUTX_L_G    = 0x22  # gyro          array start (low then high byte, each for x,y,z)
+    self.OUTX_L_XL   = 0x28  # accelerometer array start (low then high byte, each for x,y,z)
+    
     txt = 'LSM6: address {}, IDreg {}, ID {}'.\
             format(hex(self.address), hex(self.IDreg), hex(self.ID))
     print(txt)
@@ -36,16 +42,45 @@ class LSM6:
     time.sleep(0.0001)
 
   def setup(self):
-    val = self.read_unpack(self.IDreg, 1, 'B')
-    if (val[0] == self.ID):
+    val = self.read_unpack(self.WHO_AM_I, 1, 'B')
+    if (val[0] == self.DS33_WHO_ID):
       print ('LSM6 identified successfully')
     else:
       txt = 'LSM6 error: tried address {}, IDreg {}, ID should be {}, found {}'.\
-            format(self.address, self.IDreg, self.ID, val[0])
+            format(self.address, self.WHO_AM_I, self.DS33_WHO_ID, val[0])
       print (txt)
       return
-    
 
+    # all settings derived from Pololu Balancer.ino, Balance.cpp, and LSM6.cpp
+    
+    # accelerometer settings: 0b10000000 = 0x80 sets:
+    # 1.66 kHz high-performance mode (ODR = 1000, first 4 digits),
+    # (ODR means Output Data Rate)
+    # +/-2 g full scale (FS_XL = 00: next 2 digits)
+    # 400 Hz anti-aliasing filter bandwidth selection (BW_XL = 00: last 2 digits)
+    # see LSM6DS33 datasheet p. 47
+    self.write_pack(self.CTRL1_XL, 'B', 0x80)
+
+    # gyro settings: 0b01011000 = 0x58
+    # 208 Hz (normal mode, ODR = 0101, first 4)
+    # 1000 deg per sec (FS_G = 10, next 2)
+    # gyro full-scale at 125 dps: 0 = disabled (next 1 digit)
+    # last digit must be 0 in all cases
+    # see datasheet p. 48
+    self.write_pack(self.CTRL2_G, 'B', 0x58)
+
+    # common settings: 0b00000100 = 0x04
+    # 0 boot: 0 is normal mode
+    # 0 block data update: 0 is continuous update
+    # 0 interrupt activation level: 0 = interrupt output pads active high
+    # 0 push-pull mode: 0 (open-drain mode: 1)
+    # 0 SPI mode: 0 means 4-wire interface
+    # 1 register address automatically incremented during multiple-byte access: 1
+    # 0 LSB at lower address when 0
+    # 0 SW reset, 0 is normal mode
+    # see datasheet p. 49
+    self.write_pack(self.CTRL3_C, 'B', 0x40)
+    
       
 '''
   def leds(self, red, yellow, green):
