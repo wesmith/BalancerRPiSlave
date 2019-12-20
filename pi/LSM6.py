377f6b9b901889ee7c08a00e6b35a398fa678d2f
wesmith <smiwarsky@gmail.com>
2019-12-20 00:51:13 -0700

    refactoring

--- pi/LSM6.py
+++ pi/LSM6.py
@@ -5,17 +5,17 @@ WSmith copied this file from Pololu's a_star.py, to create a class to
 read the LSM6DS33 accelerometer/gyro chip on the Pololu Balboa board
 from the RPi.
 
-NOTE: unable to read block data on RPi as of 12/19/19 with existing code. Will do
-      a sledgehammer and read a byte at a time. 
+NOTE: unable to read block data on the RPi 3 as of 12/19/19 with smbus:
+      will read a byte at a time. 
 '''      
 
 import smbus
-import struct
+#import struct
 import time
 
 class LSM6:
   def __init__(self):
-    self.bus = smbus.SMBus(1)
+    self.bus         = smbus.SMBus(1)
     self.address     = 0x6b  # LSM6DS33 SAO_HIGH I2C address
     self.WHO_AM_I    = 0x0f  # register containing device ID
     self.DS33_WHO_ID = 0x69  # device ID
@@ -28,42 +28,35 @@ class LSM6:
     self.sleep       = 0.0001 # WS made a variable: time to sleep in sec between write/read
                              # (this was 0.0001 in Pololu code: 100 us)
     
-    txt = 'initializing LSM6: address {}, IDreg {}, ID {}'.\
+    txt = 'initializing LSM6: I2C address {}, ID register {}, ID value {}'.\
             format(hex(self.address), hex(self.WHO_AM_I), hex(self.DS33_WHO_ID))
     print(txt)
     self.setup()
 
   '''
-  def read_unpack(self, address, size, format):
-    self.bus.write_byte(self.address, address)
-    time.sleep(self.sleep)
-    byte_list = [self.bus.read_byte(self.address) for _ in range(size)]
-    return struct.unpack(format, bytes(byte_list))
-  '''
-  
   def write_pack(self, address, format, *data):
     data_array = list(struct.pack(format, *data))
     self.bus.write_i2c_block_data(self.address, address, data_array)
     time.sleep(self.sleep)
+  '''
 
-    
-  def read_one_byte(self, address):
-    self.bus.write_byte(self.address, address)
+  def write_one_byte(self, register, value):
+    self.bus.write_byte_data(self.address, register, value)
+    time.sleep(self.sleep)
+  
+  def read_one_byte(self, register):
+    self.bus.write_byte(self.address, register)
     time.sleep(self.sleep)
     return self.bus.read_byte(self.address)
     
-  def read_multiple_bytes(self, reg, length):
-    return [self.read_one_byte(reg + k) for k in range(length)]
+  def read_multiple_bytes(self, register, length):
+    return [self.read_one_byte(register + k) for k in range(length)]
   
-  def read_device(self, name, length): 
-    return self.read_multiple_bytes(self.choice[name], length)
+  def read_device(self, dev_name, length): 
+    return self.read_multiple_bytes(self.choice[dev_name], length)
 
-  
   def setup(self):
-    #val = self.read_unpack(self.WHO_AM_I, 1, 'B')  # 'B': unsigned char: see python struct info
     val = self.read_one_byte(self.WHO_AM_I)
-    
-    #if (val[0] == self.DS33_WHO_ID):
     if (val == self.DS33_WHO_ID):
       print ('LSM6 identified successfully')
     else:
@@ -81,7 +74,8 @@ class LSM6:
     # +/-2 g full scale (FS_XL = 00: next 2 digits)
     # 400 Hz anti-aliasing filter bandwidth selection (BW_XL = 00: last 2 digits)
     # see LSM6DS33 datasheet p. 47
-    self.write_pack(self.CTRL1_XL, 'B', 0x80)
+    self.write_one_byte(self.CTRL1_XL, 0x80)
+    #self.write_pack(self.CTRL1_XL, 'B', 0x80)
 
     # gyro settings: 0b01011000 = 0x58
     # 208 Hz (normal mode, ODR = 0101, first 4)
@@ -89,7 +83,8 @@ class LSM6:
     # gyro full-scale at 125 dps: 0 = disabled (next 1 digit)
     # last digit must be 0 in all cases
     # see datasheet p. 48
-    self.write_pack(self.CTRL2_G, 'B', 0x58)
+    self.write_one_byte(self.CTRL2_G, 0x58)
+    #self.write_pack(self.CTRL2_G, 'B', 0x58)
 
     # common settings: 0b00000100 = 0x04
     # 0 boot: 0 is normal mode
@@ -101,7 +96,8 @@ class LSM6:
     # 0 LSB at lower address when 0
     # 0 SW reset, 0 is normal mode
     # see datasheet p. 49
-    self.write_pack(self.CTRL3_C, 'B', 0x40)
+    self.write_one_byte(self.CTRL3_C, 0x40)
+    #self.write_pack(self.CTRL3_C, 'B', 0x40)
 
     time.sleep(1) # wait a second for readings to stabilize
 
