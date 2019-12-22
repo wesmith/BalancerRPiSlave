2d85caf1572116f6e99cece87a0c2e30f584e869
wesmith <smiwarsky@gmail.com>
2019-12-22 04:00:35 -0700

    started on Balancer using RPi

---
+++ pi/server_for_BalancerRPiSlave/Balboa-LSM6-RPi.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# WESmith 12/22/19
+
+# implement a non-flask testbed for communicating the lsm6 info from the RPi to the Balboa 32U4.
+
+import a_star_mod as st
+import LSM6       as ls
+import numpy      as np
+import time
+import pdb
+
+star = st.AStar()
+lsm6 = ls.LSM6()
+
+while(True):
+
+    accl = lsm6.read_device('accel')
+    gyro = lsm6.read_device('gyro')
+    
+    batt = star.read_battery_millivolts()
+    encs = star.read_encoders()
+
+    star.write_y_gyro_rate(gyro[1])
+
+    try:
+        print('batt: {} mV   encoders: {} {}'.format(batt[0], *encs))
+    except:
+        pdb.set_trace()
+
+    txt = 'accl x,y,z: {:+4d}  {:+4d}  {:+4d}   gyro x,y,z: {:+6d}  {:+6d}  {:+6d}'.\
+          format(*np.hstack([accl, gyro]))
+    print(txt)
+
+    time.sleep(0.01)
