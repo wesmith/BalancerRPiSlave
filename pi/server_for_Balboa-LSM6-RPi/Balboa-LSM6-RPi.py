017f22ef3fb63cf640efbdabe049e9f78fbf83b3
wesmith <smiwarsky@gmail.com>
2019-12-22 02:48:50 -0700

    correction

--- pi/server_for_Balboa-LSM6-RPi/Balboa-LSM6-RPi.py
+++ pi/server_for_Balboa-LSM6-RPi/Balboa-LSM6-RPi.py
@@ -4,18 +4,18 @@
 
 # implement a non-flask testbed for communicating the lsm6 info from the RPi to the Balboa 32U4.
 
-import a_star as as
+import a_star as st
 import LSM6   as ls
 import numpy  as np
 
-a_star = as.AStar()
-lsm6   = ls.LSM6()
+star = st.AStar()
+lsm6 = ls.LSM6()
 
 while(True):
 
     accl = lsm6.read_device('accel')
     gyro = lsm6.read_device('gyro')
-    batt = a_star.read_battery_millivolts()
+    batt = star.read_battery_millivolts()
 
     txt = 'batt: {:+4d}  accl x,y,z: {:+4d}  {:+4d}  {:+4d}   gyro x,y,z: {:+6d}  {:+6d}  {:+6d}'.\
           format(*np.hstack([accl, gyro]))
