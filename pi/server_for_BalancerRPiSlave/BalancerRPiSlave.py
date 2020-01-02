763c6ad88cfcfbe036bd7c003662467022eca5cc
wesmith <smiwarsky@gmail.com>
2020-01-02 01:12:32 -0700

    enabling automatic collection of data

--- pi/server_for_BalancerRPiSlave/BalancerRPiSlave.py
+++ pi/server_for_BalancerRPiSlave/BalancerRPiSlave.py
@@ -13,22 +13,19 @@ import pdb
 star = st.AStar()
 lsm6 = ls.LSM6()
 
-SLEEP = 0.001
-DATA_START_TIME = 3.0  # time to start collecting data: seconds after program start
-DATA_STOP_TIME  = 10.0 # time to stop collectiong data: seconds after program start
-# next: use z accelerometer to set start time of data collection: if z > 800 it is balancing
+DATA_TIME  = 5.0 # time in seconds to acquire data
+DELAY_TIME = 2.0 # time to delay data acquisition after balancing starts
+X_ACCEL    = 700 # the value of x_accel at which balancing has presumably commenced
+                 # (Balboa x axis is pointed vertically when perfectly balancing:
+                 #  this value is then close to 1000; it is close to 0 when Balboa is horiz)
+#SLEEP = 0.001
 
 print('Running BalancerRPiSlave.py')
-start_time = time.time()
 
 data  = []
 saved = False
 
 while(True):
-#while(k < 100):
-    
-    now   = time.time()
-    dtime = now - start_time
     
     accl = list(lsm6.read_device('accel'))
     gyro = list(lsm6.read_device('gyro'))
@@ -40,17 +37,31 @@ while(True):
     star.write_gyro_rate(*gyro) # full vector
     star.write_accel(*accl)     # full vector
 
-    if (dtime > DATA_START_TIME) and (dtime < DATA_STOP_TIME):
+    if (accl[0] < X_ACCEL): # initialize or reset: not balancing
+        START_TIMER = True
+        START_DATA  = False
+
+    if (accl[0] > X_ACCEL) and START_TIMER:
+        start_time  = time.time()
+        START_TIMER = False
+        START_DATA  = True
+
+    #if (dtime > DATA_START_TIME) and (dtime < DATA_STOP_TIME):
+    if START_DATA:
+        dtime = time.time() - start_time
         #data.append(np.hstack([dtime, accl, gyro]))
         # save time, motorX, motorY, encoderX, encoderY, gyro_rate_y
-        data.append(np.hstack([dtime, motors, encs, gyro[1]]))
-
-    if (dtime > (DATA_STOP_TIME + 1)) and (not saved):
-        fname = 'data/{}'.format(time.strftime('%Y%m%d-%H%M%S'))
-        print('saving data in {}'.format(fname))
-        data  = np.array(data)
-        saved = True
-        np.save(fname, data)
+        #data.append(np.hstack([dtime, motors, encs, gyro[1]]))
+        # is this faster?
+        if dtime > DELAY_TIME:
+            data.append([dtime, motors[0], motors[1], encs[0], encs[1], gyro[1]])
+        if dtime > (DELAY_TIME + DATA_TIME):
+            START_DATA = False
+            fname = 'data/{}'.format(time.strftime('%Y%m%d-%H%M%S'))
+            print('saving data in {}'.format(fname))
+            #data  = np.array(data)  # save without making an array: save time?
+            np.save(fname, data)
+            print('save complete')
     
     '''
     try:
