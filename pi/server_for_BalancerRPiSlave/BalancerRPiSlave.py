fbee91433f030381e499434fd0b37b556f5f2697
wesmith <smiwarsky@gmail.com>
2020-01-01 21:54:22 -0700

    added timestamp to data, refactoring

--- pi/server_for_BalancerRPiSlave/BalancerRPiSlave.py
+++ pi/server_for_BalancerRPiSlave/BalancerRPiSlave.py
@@ -14,6 +14,9 @@ star = st.AStar()
 lsm6 = ls.LSM6()
 
 SLEEP = 0.001
+DATA_START_TIME = 3.0  # time to start collecting data: seconds after program start
+DATA_STOP_TIME  = 10.0 # time to stop collectiong data: seconds after program start
+# next: use z accelerometer to set start time of data collection: if z > 800 it is balancing
 
 print('Running BalancerRPiSlave.py')
 start_time = time.time()
@@ -37,16 +40,17 @@ while(True):
     star.write_gyro_rate(*gyro) # full vector
     star.write_accel(*accl)     # full vector
 
-    if (dtime > 3) and (dtime < 10):
+    if (dtime > DATA_START_TIME) and (dtime < DATA_STOP_TIME):
         #data.append(np.hstack([dtime, accl, gyro]))
         # save time, motorX, motorY, encoderX, encoderY, gyro_rate_y
-        data.append(np.hstack([dtime, motors, encs, gyro[1]])
+        data.append(np.hstack([dtime, motors, encs, gyro[1]]))
 
-    if (dtime > 11) and (not saved):
-        print('saving data')
+    if (dtime > (DATA_STOP_TIME + 1)) and (not saved):
+        fname = time.strftime('%Y%m%d-%H%M%S')
+        print('saving data in {}'.format(fname))
         data  = np.array(data)
         saved = True
-        np.save('tmp_data', data)
+        np.save(fname, data)
     
     '''
     try:
